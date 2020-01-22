239e8c7fcfdc0301702de5060641254aa6fc7b69
Patryk <piskorz.patryk.96@gmail.com>
2020-01-22 20:12:25 +0100

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,31 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: "source-map",
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            },
+            {
                 test: /\.html$/i,
                 loader: 'html-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
         ],
     },
